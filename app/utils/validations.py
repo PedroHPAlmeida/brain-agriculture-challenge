35c829e25dfde51991f43267621a01ad58757f4f
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-11-28 21:50:32 -0300

    feat: adiciona biblioteca brutils para validação de cpf e cnpj

--- app/utils/validations.py
+++ app/utils/validations.py
@@ -1,9 +1,12 @@
+from brutils import is_valid_cnpj, is_valid_cpf
+
+
 def validate_cpf(cpf: str) -> bool:
-    return True
+    return is_valid_cpf(cpf)
 
 
 def validate_cnpj(cnpj: str) -> bool:
-    return True
+    return is_valid_cnpj(cnpj)
 
 
 def is_area_smaller_than_areable_and_vegetation(total_area: float, arable_area: float, vegetation_area: float) -> bool:
