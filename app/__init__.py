f607df45ff7e2bf412e98a6e96f901c961b94165
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-11-28 21:56:17 -0300

    feat: adiciona parâmetros de descrição da API

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 from .controllers import farm_router, farmer_router
 
-app = FastAPI()
+app = FastAPI(title="Brain Agriculture", summary="Brain Agriculture Management System", version="1.0.0")
 app.include_router(farmer_router)
 app.include_router(farm_router)
